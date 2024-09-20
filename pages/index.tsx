5e01833494a6cfc4f09c2850dea5d238b377b7ab
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-09-20 17:03:23 +0900

    120: feat: refactor pages componenrs

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,22 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
 	return (
-		<Stack className={"Home-page"}>
+    <Stack className={"Home-page"}>
+      			<Stack>
+				<Stack className="container">Trend Properties</Stack>
+			</Stack>
 			<Stack>
 				<Stack className="container">Popular Properties</Stack>
+      </Stack>
+      			<Stack>
+				<Stack className="container"> Advertisement</Stack>
 			</Stack>
 			<Stack>
 				<Stack className="container">Top Agent</Stack>
 			</Stack>
 			<Stack>
 				<Stack className="container">Top Properties</Stack>
-			</Stack>
+      </Stack>
 		</Stack>
 	);
 };
