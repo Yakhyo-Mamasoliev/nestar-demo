7e586c244c95dcef623b1a536257180d0eea7961
Yakhyo-Mamasoliev <yakhyodsu@gmail.com>
2024-09-20 18:35:18 +0900

    121: feat: develop homepage components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,25 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import {  Stack } from "@mui/material";
 import { NextPage } from "next";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-const Home: NextPage = () => {
-	return (
-    <Stack className={"Home-page"}>
-      			<Stack>
-				<Stack className="container">Trend Properties</Stack>
-			</Stack>
-			<Stack>
-				<Stack className="container">Popular Properties</Stack>
+const  Home: NextPage = () => {
+  return (
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
       </Stack>
-      			<Stack>
-				<Stack className="container"> Advertisement</Stack>
-			</Stack>
-			<Stack>
-				<Stack className="container">Top Agent</Stack>
-			</Stack>
-			<Stack>
-				<Stack className="container">Top Properties</Stack>
-      </Stack>
-		</Stack>
-	);
-};
+  );
+}
 
-export default withLayoutMain(Home);
+export default withLayoutMain(Home)
